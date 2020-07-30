bb86695f5be5d34c7cef806baca019b61fb8164b
WIN-LIM7UQIUOJI\User <belorusochkaYul@gmail.com>
2020-07-30 17:30:05 +0300

    Add task number 2

---
+++ cw222/src/by/kiparo/cd/main.java
@@ -0,0 +1,33 @@
+package by.kiparo.cd;
+
+public class main {
+public static void main(String[] arg) {
+    int x = 12476797;
+    int last= x%10;
+
+if (last==7) {
+    System.out.print("у вас счастливый номер=");
+    System.out.print(last);
+                             }
+else
+{
+    System.out.println("попробуйте еще раз");
+                                              }
+
+
+
+                                                   }
+
+                                                }
+
+
+
+
+
+
+
+
+
+
+
+
