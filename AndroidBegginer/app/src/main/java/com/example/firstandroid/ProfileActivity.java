58d89407da818e1e08e6e8ac8d9f31f40ad5f254
your nameJuly <belorusochkayul@gmail.com>
2021-04-06 18:00:42 +0300

    Add task with download image from int

--- AndroidBegginer/app/src/main/java/com/example/firstandroid/ProfileActivity.java
+++ AndroidBegginer/app/src/main/java/com/example/firstandroid/ProfileActivity.java
@@ -14,10 +14,10 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        String imageUri = "https://i.imgur.com/tGbaZCY.jpg";
-        ImageView ivBasicImage = (ImageView) findViewById(R.id.profilePhoto);
+        String imageUrl = "https://i.imgur.com/tGbaZCY.jpg";
+        ImageView userImageView = (ImageView) findViewById(R.id.profilePhoto);
         Picasso.get()
-                .load(imageUri)
-                .into(ivBasicImage);
+                .load(imageUrl)
+                .into(userImageView);
     }
 }
