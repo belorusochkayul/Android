2dc4a9f6b3ed195e079dc1d72056fc56ca927c38
belorusochkayul <68927630+belorusochkayul@users.noreply.github.com>
2021-04-09 18:06:36 +0300

    Add files via upload

--- AndroidBeginner/app/src/main/java/com/example/firstandroid/MenuActivity.java
+++ AndroidBeginner/app/src/main/java/com/example/firstandroid/MenuActivity.java
@@ -5,34 +5,60 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.ScrollView;
 
 public class MenuActivity extends Activity {
-    Button buttonTextChange, buttonFlags, buttonNextTask;
-    ScrollView scrollView;
-    LinearLayout linearLayout;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        buttonTextChange = (Button) findViewById(R.id.buttonTextExchange);
-        linearLayout = new LinearLayout(this);
-        scrollView = new ScrollView(this);
+        Button buttonTextChange = (Button) findViewById(R.id.buttonTextExchange);
+        Button buttonFlags = (Button) findViewById(R.id.buttonFlags);
+        Button buttonLogin = (Button) findViewById(R.id.buttonLogin);
+        Button buttonImageDownload = (Button) findViewById(R.id.buttonImage);
+        Button buttonProfile = (Button) findViewById(R.id.profilePhoto);
 
         buttonTextChange.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                switch (v.getId()) {
-                    case R.id.buttonTextExchange: {
-                        Intent intent = new Intent(MenuActivity.this, TextChangeActivity.class);
-                        startActivity(intent);
-                        break;
-                    }
-                }
+                Intent intent = new Intent(MenuActivity.this, TextChangeActivity.class);
+                startActivity(intent);
             }
         });
+        buttonFlags.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, FlagsActivity.class);
+                startActivity(intent);
+            }
+        });
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, LoginLinearActivity.class);
+                startActivity(intent);
+            }
+        });
+        buttonImageDownload.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, ImageActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonProfile.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
